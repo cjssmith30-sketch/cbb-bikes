7a38b67cc53d8f49decd241cb5240190a571f6f1
Christopher Smith <cjs.smith.30@gmail.com>
2026-04-21 22:19:24 -0600

    Add Railway DB port and SSL config

--- netlify/functions/manager-reports.js
+++ netlify/functions/manager-reports.js
@@ -5,10 +5,11 @@ const crypto = require('crypto');
 
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true },
+  ssl: { rejectUnauthorized: false },
 };
 
 function verifyToken(authHeader) {
