2f2d28d066b660735e43eeff5003f5ad26fcaabd
Christopher Smith <cjs.smith.30@gmail.com>
2026-04-21 22:48:48 -0600

    Fix syntax error in dbConfig

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -7,8 +7,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT),
-  ssl: { rejectUnauthorized: false },,
+  ssl: { rejectUnauthorized: false },
 };
 
 exports.handler = async (event) => {
--- netlify/functions/manager-reports.js
+++ netlify/functions/manager-reports.js
@@ -9,8 +9,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT),
-  ssl: { rejectUnauthorized: false },,
+  ssl: { rejectUnauthorized: false },
 };
 
 function verifyToken(authHeader) {
